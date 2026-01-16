240073839a2598a52ac400d0b7f5f758254742ad
Hamim Mahmud <hamimmahmud0@gmail.com>
2026-01-16 16:27:52 +0600

    mdns added

--- main/main.c
+++ main/main.c
@@ -14,11 +14,77 @@
 #include <esp_system.h>
 #include <esp_event.h>
 
+#include <mdns.h>
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
 #include <nvs_flash.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 #include <freertos/task.h>
+
+static void camwebsrv_mdns_start(camwebsrv_cfgman_t cfgman)
+{
+  esp_err_t rv;
+  const char *pair_id = "0";
+  const char *role = "master";
+  char hostname[64];
+
+  // Optional config keys (fall back to defaults if missing)
+  rv = camwebsrv_cfgman_get(cfgman, CAMWEBSRV_CFGMAN_KEY_PAIR_ID, &pair_id);
+  if (rv != ESP_OK)
+  {
+    pair_id = "0";
+  }
+
+  rv = camwebsrv_cfgman_get(cfgman, CAMWEBSRV_CFGMAN_KEY_ROLE, &role);
+  if (rv != ESP_OK)
+  {
+    role = "master";
+  }
+
+  // Build hostname: cam-master-<pair_id> or cam-slave-<pair_id>
+  // mDNS hostnames should be letters/digits/hyphen; sanitize pair_id just in case.
+  char pair_s[24];
+  size_t j = 0;
+  for (size_t i = 0; pair_id[i] != 0x00 && j < sizeof(pair_s) - 1; i++)
+  {
+    unsigned char c = (unsigned char)pair_id[i];
+    if (isalnum(c))
+    {
+      pair_s[j++] = (char)tolower(c);
+    }
+    else if (c == '-' || c == '_')
+    {
+      pair_s[j++] = '-';
+    }
+    else
+    {
+      pair_s[j++] = '-';
+    }
+  }
+  pair_s[j] = 0x00;
+  if (pair_s[0] == 0x00)
+  {
+    strcpy(pair_s, "0");
+  }
+
+  bool is_master = (strcasecmp(role, "master") == 0);
+  snprintf(hostname, sizeof(hostname), is_master ? "cam-master-%s" : "cam-slave-%s", pair_s);
+
+  ESP_ERROR_CHECK(mdns_init());
+  ESP_ERROR_CHECK(mdns_hostname_set(hostname));
+  ESP_ERROR_CHECK(mdns_instance_name_set(hostname));
+
+  // Advertise the HTTP server.
+  // Note: ESP-IDF appends .local automatically when resolving the hostname.
+  ESP_ERROR_CHECK(mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0));
+
+  ESP_LOGI(CAMWEBSRV_TAG, "MDNS started: http://%s.local/", hostname);
+}
 
 void app_main()
 {
@@ -101,6 +167,9 @@ void app_main()
     ESP_LOGE(CAMWEBSRV_TAG, "MAIN app_main(): camwebsrv_wifi_init() failed: [%d]: %s", rv, esp_err_to_name(rv));
     goto camwebsrv_main_error;
   }
+
+  // Start mDNS after Wi-Fi is connected (camwebsrv_wifi_init blocks until got IP).
+  camwebsrv_mdns_start(cfgman);
 
   // initialise ping
 
